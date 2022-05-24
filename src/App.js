b8f945f9c834d97f2c409d3748007f970271b6c0
Van Drie Joshua D <VanDrieJoshua@JohnDeere.com>
2022-05-24 13:42:36 -0500

    add basic non-dynamic card

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero"
+import Card from "./components/Card"
 import './style.css'
 /*
 Challenge: Build the Navbar component.
@@ -12,6 +13,7 @@ export default function App() {
         <div>
             <Navbar/>
             <Hero/>
+            <Card/>
         </div>
     )
 }
