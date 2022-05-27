eb470e75de36ec47a3d57b78c7a7652782305ed3
Van Drie Joshua D <VanDrieJoshua@JohnDeere.com>
2022-05-27 10:26:57 -0500

    add dynamic cards

--- src/App.js
+++ src/App.js
@@ -3,17 +3,31 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero"
 import Card from "./components/Card"
 import './style.css'
+import Data from './data'
 /*
 Challenge: Build the Navbar component.
 Check the Figma file for the design specifics.
 */
 
 export default function App() {
+    const cards = Data.map(props => {
+        return <Card
+            key={props.id}
+            coverImg={props.coverImg}
+            rating={props.stats.rating}
+            reviewCount={props.stats.reviewCount}
+            title={props.title}
+            location={props.location}
+            price={props.price}
+            />
+    })
     return (
         <div>
             <Navbar/>
             <Hero/>
-            <Card/>
+            <section className="cards-list">
+                {cards}
+            </section>
         </div>
     )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,37 +1,18 @@
 import React from "react"
-import Katie from '../images/katie_zaferes.png'
 import Star from '../images/star.png'
-/*
-Challenge: Build the Card component
-For now, hard-code in the data (like
-the rating, title, price, etc.)
 
-Notes:
-- Only render 1 instance (I already did this for you)
-- The star icon and photo (katie-zaferes.png) are in the images
-  folder for your use
-- Make sure to include:
-    - image
-    - star icon (star.png), rating, and review count
-    - title
-    - cost/person
-- The main purpose of this challenge is to show you where our limitations
-  currently are, so don't worry about the fact that you're hard-coding all
-  this data into the component.
-*/
-
-export default function Card() {
+export default function Card(props) {
     return (
         <div className="card">
-            <img src={Katie} className="card--image"/>
+            <img src={require('../images/' +props.coverImg+'.png')} className="card--image" alt={props.description}/>
             <div className="card--stats">
                 <img src={Star} className="card--star"/>
-                <span>5.0</span>
-                <span className="gray">(6) • </span>
-                <span className="gray">USA</span>
+                <span>{props.rating}</span>
+                <span className="gray">({props.reviewCount}) •</span>
+                <span className="gray">{props.location}</span>
             </div>
-            <p>Life lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136 </span>/ Person</p>
+            <p className="card--title">{props.title}</p>
+            <p className="card--price"><span className="bold">From ${props.price} </span>/ Person</p>
         </div>
     )
 }
